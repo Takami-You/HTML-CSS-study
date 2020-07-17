c347d04c5e9fd7d9993cdae866a489267b6c4c08
Takami-You <60865757+Takami-You@users.noreply.github.com>
2020-07-17 16:44:59 +0800

    Add files via upload

---
+++ ll公式JS.js
@@ -0,0 +1,33 @@
+function mouseOver1()
+{
+	document.getElementById('a1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/yuigaoka_on.png"
+}
+function mouseOut1()
+	{
+	document.getElementById('a1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/yuigaoka_off.png"
+	}
+
+function mouseOver2()
+{
+	document.getElementById('b1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/nijigasaki_on.png"
+}
+function mouseOut2()
+	{
+	document.getElementById('b1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/nijigasaki_off.png"
+	}
+
+function mouseOver3()
+{
+	document.getElementById('c1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/%CE%BCs_on.png"}
+function mouseOut3()
+	{
+	document.getElementById('c1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/%CE%BCs_off.png"
+	}	
+function mouseOver4()
+{
+	document.getElementById('d1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/sunshine_on.png"
+}
+function mouseOut4()
+	{
+	document.getElementById('d1').src ="http://www.lovelive-anime.jp/js/lovelive.hyperesources/sunshine_off.png"
+	}	
