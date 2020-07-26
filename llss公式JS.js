84cef2aee274da31b0e05f99e041896628a6b82a
Takami-You <60865757+Takami-You@users.noreply.github.com>
2020-07-26 18:04:51 +0800

    ラブライブ！サンシャイン!! Official Web Site复刻
    基本功能实现，访问推特的API实在是搞不上

---
+++ llss公式JS.js
@@ -0,0 +1,109 @@
+function mouseOver11()
+	{
+		document.getElementById('a11').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu13_on.png"
+	}
+function mouseOut11()
+	{
+		
+		document.getElementById('a11').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu13_off.png"
+	}
+
+function mouseOver10()
+	{
+		document.getElementById('a10').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu12_on.png"
+	}
+function mouseOut10()
+	{
+		
+		document.getElementById('a10').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu12_off.png"
+	}
+
+function mouseOver9()
+	{
+		document.getElementById('a9').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu11_on.png"
+	}
+function mouseOut9()
+	{
+		
+		document.getElementById('a9').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu11_off.png"
+	}
+
+function mouseOver8()
+	{
+		document.getElementById('a8').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu10_on.png"
+	}
+function mouseOut8()
+	{
+		
+		document.getElementById('a8').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu10_off.png"
+	}
+
+function mouseOver7()
+	{
+		document.getElementById('a7').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu09_on.png"
+	}
+function mouseOut7()
+	{
+		
+		document.getElementById('a7').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu09_off.png"
+	}
+
+function mouseOver6()
+	{
+		document.getElementById('a6').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu08_on.png"
+	}
+function mouseOut6()
+	{
+		
+		document.getElementById('a6').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu08_off.png"
+	}
+
+function mouseOver5()
+	{
+		document.getElementById('a5').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu07_on.png"
+	}
+function mouseOut5()
+	{
+		
+		document.getElementById('a5').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu07_off.png"
+	}
+
+function mouseOver4()
+	{
+		document.getElementById('a4').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu06_on.png"
+	}
+function mouseOut4()
+	{
+		
+		document.getElementById('a4').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu06_off.png"
+	}
+
+function mouseOver3()
+	{
+		document.getElementById('a3').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu05_on.png"
+	}
+function mouseOut3()
+	{
+		
+		document.getElementById('a3').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu05_off.png"
+	}
+
+function mouseOver2()
+	{
+		document.getElementById('a2').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu03_on.png"
+	}
+function mouseOut2()
+	{
+		
+		document.getElementById('a2').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu03_off.png"
+	}
+
+function mouseOver1()
+	{
+		document.getElementById('a1').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu01_on.png"
+	}
+function mouseOut1()
+	{
+		
+		document.getElementById('a1').src ="http://www.lovelive-anime.jp/uranohoshi/img/common/menu01_off.png"
+	}
